e43af1bcbac7597cc172a3bbd00fe21c6a69db4d
fabri <development.for.code@gmail.com>
2025-11-06 16:34:55 +0100

    Update-comments

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,25 +1,14 @@
 
 let parolaInserita = prompt("inserisci la parola");
 
-
-
 function checkPalindrome(str) {
 
 parolaInserita = str.split("").reverse().join("");
 
-
-
-    
-
     if (parolaInserita == str){
-
         return true;
     }
-
     return false;
-    
-
-
 }
 
 checkPalindrome(parolaInserita);
--- Pari-o-Dispari/script.js
+++ Pari-o-Dispari/script.js
@@ -1,21 +1,29 @@
 
-let pariOrDispari = prompt("Scegli pari o dispari");
+//Introduzione
+let pariOrDispari = prompt("Scegli pari o dispari"); //creo un prompt per scrivere
+//pari o dispari
 
-  
-    while (pariOrDispari.toLowerCase() !== "pari" && pariOrDispari.toLowerCase() !== "dispari" ){ //se la stringa 
-                                                                                            //è diversa da pari o
-                                                                                            //dispari fai alert
-        alert("Scegli 'Pari' o 'Dispari'");
+//Ciclo che trasforma in minuscolo ciò che viene inserito nel prompt
+while (pariOrDispari.toLowerCase() !== "pari" &&
+    pariOrDispari.toLowerCase() !== "dispari") { //se la stringa è diversa da pari o
+    //dispari crea un alert
+
+    alert("Scegli 'Pari' o 'Dispari'");
 
     pariOrDispari = prompt("Devi Scegle pari o dispari!");
-    
+
 }
 
-let numeroInseritoString = prompt("Inserisci un numero da 1 a 5");
-let numeroInserito = parseInt(numeroInseritoString);
+let numeroInseritoString = prompt("Inserisci un numero da 1 a 5"); //creo un altro prompt,
+//questa volta per inserire un numero
+//compreso tra 1 e 5
 
+let numeroInserito = parseInt(numeroInseritoString); //converto in intero la stringa che ho
+//scritto nel prompt
 
-while (numeroInserito <1 || numeroInserito >5 || isNaN(numeroInserito)) {
+//Ciclo che verifica se ciò che inserisco nel prompt non sia un numero inferiore a 1,
+//un numero inferiore a 5, o una stringa
+while (numeroInserito < 1 || numeroInserito > 5 || isNaN(numeroInserito)) {
 
     alert("Inserisci un numero da 1 a 5");
 
@@ -23,22 +31,23 @@ while (numeroInserito <1 || numeroInserito >5 || isNaN(numeroInserito)) {
 }
 
 
+//Svolgimento
 
-function randomNumber () {
+//Creo una funzione che mi crea un numero casuale
+function randomNumber() {
 
-    let numRan = Math.floor(Math.random() *5) + 1;
-   return numRan;
+    let numRan = Math.floor(Math.random() * 5) + 1;
+    return numRan;
 }
 
-let random = randomNumber();
+let random = randomNumber(); //assegno la funzione ad una variabile
+let somma = random + numeroInserito; // creo una variabile che mi somma il numero 
+// da 1 a 5 inserito, e il numero casuale creato dalla funzione 
 
-    
-let somma = random + numeroInserito;
+console.log(somma); //stampo la somma in console
 
-console.log(somma);
-
-
-function sommaPariOdispari (){
+//Creo una funzione che verifica se il numero inserito dall'utente sia pari o dispari
+function sommaPariOdispari() {
 
     if (pariOrDispari == "pari" && somma % 2 == 0) {
 
@@ -52,24 +61,23 @@ function sommaPariOdispari (){
 
     }
 
-     else if (pariOrDispari == "pari" && somma % 2 != 0) {
+    else if (pariOrDispari == "pari" && somma % 2 != 0) {
 
         alert("Purtroppo hai perso.");
 
     }
 
-      else if (pariOrDispari == "dispari" && somma % 2 == 0) {
+    else if (pariOrDispari == "dispari" && somma % 2 == 0) {
 
         alert("Purtroppo hai perso.");
 
     }
-
-
 
 }
 
+//Conclusione
+sommaPariOdispari(); //invoco (richiamo) la funzione
 
-
-console.log("il numero random è: " + random);
-console.log("il numero inserito è: " + numeroInserito);
-console.log("la somma è: " + somma);
+console.log("il numero random è: " + random); //stampo in console il numero random
+console.log("il numero inserito è: " + numeroInserito); //stampo in console il numero inserito dall'utente
+console.log("la somma è: " + somma); //stampo in console la somma
